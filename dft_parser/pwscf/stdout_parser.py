efcfae778f8245ce05948f7259b66e55f3762a92
Max Hutchinson <maxhutch@gmail.com>
2017-08-22 07:01:24 -0500

    Fixing super in 2/3

--- dft_parser/pwscf/stdout_parser.py
+++ dft_parser/pwscf/stdout_parser.py
@@ -1,5 +1,4 @@
 from ..core import BlockParser
-from builtins import super
 
 def _parse_lattice_parameter(line, lines):
     return {"lattice parameter": float(line.split()[4])}
@@ -111,7 +110,7 @@ base_rules = [
 class PwscfStdOutputParser(BlockParser):
 
     def __init__(self, rules=base_rules):
-        super().__init__()
+        BlockParser.__init__(self)
         for rule in rules:
             self.add_rule(rule)
 
