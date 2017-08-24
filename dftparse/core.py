3adb8c6aa896f6a0bdafc4dc03cd9309e8cee8df
Max Hutchinson <maxhutch@gmail.com>
2017-08-23 19:50:44 -0500

    Using iter() and adding docs

--- dftparse/core.py
+++ dftparse/core.py
@@ -3,13 +3,16 @@ class BlockParser(object):
     """Parser built on rules that parse blocks of input"""
 
     def __init__(self, rules=[]):
+        """Create a BlockParser, pre-loading a set of rules"""
         self.rules = rules
 
     def add_rule(self, rule):
+        """Add a rule to this parser"""
         self.rules.append(rule)
 
     def parse(self, generator):
-        gen = generator.__iter__()
+        """Parse an iterable source of strings into a generator"""
+        gen = iter(generator)
         for line in gen:
             block = {}
             for rule in self.rules:
