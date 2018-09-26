52b3009026478e3a12eddf5488bb7ae9e80854e3
Vinay Hegde <hegdevinayi@gmail.com>
2018-09-26 16:37:55 -0500

    Add unit test for parsing volume of cell from OUTCAR

--- dftparse/vasp/tests/test_outcar_parser.py
+++ dftparse/vasp/tests/test_outcar_parser.py
@@ -19,3 +19,16 @@ def test_parse_magnetization():
     res = _flatten(OutcarParser().parse(lines))
     assert res["total magnetization"] == -0.0000036, "Parsed the total magnetization incorrectly"
     assert res["number of electrons"] == 12.9999995, "Parsed the number of electrons incorrectly"
+
+
+def test_parse_volume_of_cell():
+    """Test that the volume of cell parses out correctly"""
+    lines = """
+ VOLUME and BASIS-vectors are now :
+ -----------------------------------------------------------------------------
+  energy-cutoff  :      650.00
+  volume of cell :       22.75
+      direct lattice vectors                 reciprocal lattice vectors
+    """.split("\n")
+    res = _flatten(OutcarParser().parse(lines))
+    assert res["volume of cell"] == 22.75, "Parsed the volume of cell incorrectly"
